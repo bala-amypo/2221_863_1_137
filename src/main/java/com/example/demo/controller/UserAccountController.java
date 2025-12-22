f6ac274145a6cd7a55a5ca8823739357f3b4a3b9
bala-amypo <bala@amypo.in>
2025-12-22 02:53:59 +0000

    User commit

--- src/main/java/com/example/demo/controller/UserAccountController.java
+++ src/main/java/com/example/demo/controller/UserAccountController.java
@@ -13,36 +13,30 @@ import com.example.demo.service.UserAccountService;
 public class UserAccountController {
 
     @Autowired
-    private UserAccountService uas;
+    private UserAccountService userAccountService;
 
     @PostMapping
-    public UserAccount createUser(@RequestBody UserAccount user) {
-        return uas.createUser(user);
+    public UserAccount createUser(@RequestBody UserAccount userAccount) {
+        return userAccountService.createUser(userAccount);
     }
 
     @GetMapping
     public List<UserAccount> getAllUsers() {
-        return uas.getAllUsers();
+        return userAccountService.getAllUsers();
     }
 
-    // @GetMapping("/{id}")
-    // public UserAccount getUserById(@PathVariable long id) {
-    //     return uas.getUserById(id)
-    //             .orElseThrow(() -> new RuntimeException("User not found"));
-    // }
     @GetMapping("/{id}")
-    public UserAccount getUserById(@PathVariable long id) {
-        return uas.getUserById(id);
+    public UserAccount getUserById(@PathVariable Long id) {
+        return userAccountService.getUserById(id);
     }
-
 
     @PutMapping("/{id}")
-    public UserAccount updateUser(@PathVariable long id, @RequestBody UserAccount user) {
-        return uas.updateUser(id, user);
+    public UserAccount updateUser(@PathVariable Long id, @RequestBody UserAccount userAccount) {
+        return userAccountService.updateUser(id, userAccount);
     }
 
     @PutMapping("/{id}/deactivate")
-    public void deactivateUser(@PathVariable long id) {
-        uas.deactivateUser(id);
+    public UserAccount deactivateUser(@PathVariable Long id) {
+        return userAccountService.deactivateUser(id);
     }
 }
